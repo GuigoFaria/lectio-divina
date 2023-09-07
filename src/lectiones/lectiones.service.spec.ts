c894d36be2ec6f04e92e58555b35e2f176b058ba
Guilherme Raulino de Faria <guilhermeraulino2704@gmail.com>
2023-09-06 22:25:25 -0300

    feat: full tested lectione service

--- src/lectiones/lectiones.service.spec.ts
+++ src/lectiones/lectiones.service.spec.ts
@@ -10,6 +10,16 @@ import { mockUserRepository } from 'src/users/mocks/UserRepository.mock';
 import { mockLectioneRepository } from './mocks/LectioneRepository.mock';
 
 describe('LectionesService', () => {
+  const user: User = {
+    createdAt: new Date('01-01-2022'),
+    email: 'email',
+    id: '1',
+    name: 'name',
+    password: 'password',
+    sequenceDays: 1,
+    timeDisplayedInSeconds: 1,
+    updatedAt: new Date('01-01-2022'),
+  };
   let service: LectionesService;
   let repositoryLectione: Repository<Lectione>;
   let repositoryUser: Repository<User>;
@@ -49,16 +59,7 @@ describe('LectionesService', () => {
       prayerTime: 10,
       userId: '1',
     };
-    const user: User = {
-      createdAt: new Date('01-01-2022'),
-      email: 'email',
-      id: '1',
-      name: 'name',
-      password: 'password',
-      sequenceDays: 1,
-      timeDisplayedInSeconds: 1,
-      updatedAt: new Date('01-01-2022'),
-    };
+
     const expectedReturn = {
       practicalResolution: createDto.practicalResolution,
       prayerTime: createDto.prayerTime,
@@ -74,5 +75,90 @@ describe('LectionesService', () => {
     });
     expect(repositoryLectione.create).toHaveBeenCalledWith(expectedReturn);
     expect(repositoryLectione.save).toHaveBeenCalledWith(expectedReturn);
+  });
+  it('should not create a lectione', async () => {
+    const createDto: CreateLectioneDto = {
+      text: 'text',
+      totalTime: 10,
+      practicalResolution: 'practicalResolution',
+      prayerTime: 10,
+      userId: '',
+    };
+    const spyFindOne = jest
+      .spyOn(repositoryUser, 'findOne')
+      .mockRejectedValue(null);
+    try {
+      await service.create(createDto);
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      expect(spyFindOne).toHaveBeenCalledWith({
+        where: { id: createDto.userId },
+      });
+    }
+  });
+
+  it('should find all lectiones by user', async () => {
+    const expectedLectiones: Lectione[] = [
+      {
+        id: '1',
+        updatedAt: new Date(),
+        createdAt: new Date(),
+        practicalResolution: 'practicalResolution',
+        prayerTime: 10,
+        text: 'text',
+        totalTime: 10,
+        user: user,
+      },
+    ];
+    const spyFind = jest
+      .spyOn(repositoryLectione, 'find')
+      .mockResolvedValue([...expectedLectiones]);
+
+    const result = await service.findAllByUser('1');
+    expect(spyFind).toHaveBeenCalledWith({ where: { user: { id: '1' } } });
+    expect(result).toStrictEqual(expectedLectiones);
+  });
+
+  it('should not find all lectiones by user', async () => {
+    const spyFind = jest
+      .spyOn(repositoryLectione, 'find')
+      .mockRejectedValue(new Error());
+    try {
+      await service.findAllByUser('1');
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      expect(spyFind).toHaveBeenCalledWith({ where: { user: { id: '1' } } });
+    }
+  });
+
+  it('should find one lectione by id', async () => {
+    const expectedLectione: Lectione = {
+      id: '1',
+      updatedAt: new Date(),
+      createdAt: new Date(),
+      practicalResolution: 'practicalResolution',
+      prayerTime: 10,
+      text: 'text',
+      totalTime: 10,
+      user: user,
+    };
+    const spyFind = jest
+      .spyOn(repositoryLectione, 'findOne')
+      .mockResolvedValue(expectedLectione);
+
+    const result = await service.findOne('1');
+    expect(spyFind).toHaveBeenCalledWith({ where: { id: '1' } });
+    expect(result).toStrictEqual(expectedLectione);
+  });
+  it('should not find all lectiones by user', async () => {
+    const spyFind = jest
+      .spyOn(repositoryLectione, 'findOne')
+      .mockRejectedValue(new Error());
+    try {
+      await service.findAllByUser('1');
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      expect(spyFind).toHaveBeenCalledWith({ where: { id: '1' } });
+    }
   });
 });
--- src/lectiones/mocks/LectioneRepository.mock.ts
+++ src/lectiones/mocks/LectioneRepository.mock.ts
@@ -1,5 +1,6 @@
 export const mockLectioneRepository = {
   findOne: jest.fn((entity) => entity),
   create: jest.fn((CreateLectioneDto) => CreateLectioneDto),
+  find: jest.fn((id) => id),
   save: jest.fn((Lectione) => Lectione),
 };
